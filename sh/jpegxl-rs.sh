0001400d0c4d9577c21b05c8cdfdc37fe9021ed4
any <any@wac.tax>
2023-08-13 06:29:38 +0800

    ♨

--- sh/jpegxl-rs.sh
+++ sh/jpegxl-rs.sh
@@ -7,16 +7,15 @@ os_type=$(uname)
 
 source $DIR/sh/cflag.sh
 
-VER=0.8.2
+# VER=0.8.2
+#
+# if [ ! -d "jpegxl-rs" ]; then
+#   git clone --recursive --shallow-submodules --depth=1 https://github.com/inflation/jpegxl-rs.git
+# fi
 
-if [ ! -d "jpegxl-rs" ]; then
-  git clone --recursive --shallow-submodules --depth=1 https://github.com/inflation/jpegxl-rs.git
-fi
-
-cd jpegxl-rs/jpegxl-src/libjxl
-git checkout main
-git pull
-git reset --hard origin/main
+# cd jpegxl-rs/jpegxl-src/libjxl
+git clone --depth=1 https://github.com/libjxl/libjxl.git
+cd libjxl
 git submodule update --init --recursive
 
 ./deps.sh && cmake . -DBUILD_TESTING=OFF && make -j $(nproc) && make install
