ca4ee80c67f39294485c44f1ca3dd4879e95e05a
xxai.art <xxai.art@gmail.com>
2023-08-27 20:29:24 +0800

    ♨

--- img.sh
+++ img.sh
@@ -2,4 +2,4 @@
 
 nc -z -w 1 127.0.0.1 7890 && export https_proxy=http://127.0.0.1:7890
 
-TO="https://f004.backblazeb2.com/file/xxai-jxl/" RUST_BACKTRACE=short exec /usr/local/bin/img
+TO=https://user0.gq/ RUST_BACKTRACE=short exec /usr/local/bin/img
