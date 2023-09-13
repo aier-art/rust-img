cef018e68261d2cee0deebf3011bc6ed01d7187d
xxai.art <xxai.art@gmail.com>
2023-09-13 14:44:53 +0800

    ♨

--- img.sh
+++ img.sh
@@ -2,4 +2,4 @@
 
 nc -z -w 1 127.0.0.1 7890 && export https_proxy=http://127.0.0.1:7890
 
-TO=https://xxai.eu.org/ RUST_BACKTRACE=short exec /opt/bin/img
+PORT=5500 TO=https://xxai.eu.org/ RUST_BACKTRACE=short exec /opt/bin/img
